bac334dfc5fca03139096bca8c6beeadd284abce
Jonathon <jonathon.nordquist@gmail.com>
2016-04-06 09:52:16 -0700

    Refactor of get image behavoir

--- app/helpers/tumblr_api_helper.rb
+++ app/helpers/tumblr_api_helper.rb
@@ -6,23 +6,41 @@ module TumblrApiHelper
     return ["catsncats.tumblr.com"]
   end
 
-  def get_image_url(source, number)
+  def config_tumblr
     Tumblr.configure do |config|
       config.consumer_key = Rails.application.secrets.tumblr_consumer_key
       config.consumer_secret = Rails.application.secrets.tumblr_consumer_secret
       config.oauth_token = Rails.application.secrets.tumblr_oauth_token
       config.oauth_token_secret = Rails.application.secrets.tumblr_oauth_token_secret
     end
+  end
 
+  def get_tumblr_account(accounts)
+    target = Random.rand(accounts.length)
+    return accounts[target]
+  end
+
+  def get_image_url(source, number)
     client = Tumblr::Client.new
 
-    postingAccount = client.blog_info("catsncats.tumblr.com")
+    postingAccount = client.blog_info(source)
     target = Random.rand(postingAccount["blog"]["total_posts"] - 1)
 
 
     post = client.posts("catsncats.tumblr.com", :limit => 1, :offset => target)
 
-    return post["posts"][0]["photos"][0]["original_size"]["url"]
+    # return post["posts"][0]["photos"][0]["original_size"]["url"]
+    return post["posts"]
+  end
+
+
+  def get_image_alg
+    config_tumblr
+    accounts = get_tumblr_sources
+    account = get_tumblr_account(accounts)
+    get_image_url(account, 1)
+
+
   end
 
 end
