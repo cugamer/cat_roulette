3d92a1392f60b2da1793e5f112a9ef308900a3f8
Jonathon <jonathon.nordquist@gmail.com>
2016-04-06 12:41:16 -0700

    Refactor controller logic into CatPagesHelper

--- app/controllers/cat_pages_controller.rb
+++ app/controllers/cat_pages_controller.rb
@@ -1,10 +1,10 @@
 class CatPagesController < ApplicationController
 	def index
 		# render plain: get_image_metadata_alg
-		metadata = get_image_metadata_conf_pic
-		@image_url = metadata[0]["photos"][0]["original_size"]["url"]
-		@blog_name = metadata[0]["blog_name"]
-		@post_url = metadata[0]["post_url"]
+		@metadata = get_image_metadata_conf_pic
+		# @image_url = metadata[0]["photos"][0]["original_size"]["url"]
+		# @blog_name = metadata[0]["blog_name"]
+		# @post_url = metadata[0]["post_url"]
 
 		render 'index'
 	end
--- app/helpers/cat_pages_helper.rb
+++ app/helpers/cat_pages_helper.rb
@@ -1,11 +1,13 @@
 module CatPagesHelper
-	def get_post_blog_name
-
+	def get_post_blog_name(metadata)
+		return metadata[0]["blog_name"]
 	end
 
-	def get_post_image_url
-
+	def get_post_url(metadata)
+		return metadata[0]["post_url"]
 	end
 
-	def get_post_blog_url
+	def get_post_image_url(metadata)
+		return metadata[0]["photos"][0]["original_size"]["url"]
+	end
 end
