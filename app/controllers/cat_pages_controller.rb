89e610705adabc9b3e84e5a0127ae272108ceb84
Jonathon <jonathon.nordquist@gmail.com>
2016-04-17 15:44:43 -0700

    Create serveSingleImage method in CatPagesController, add route and update ApplicationController to use CatPagesHelper.

--- app/controllers/cat_pages_controller.rb
+++ app/controllers/cat_pages_controller.rb
@@ -1,10 +1,18 @@
 class CatPagesController < ApplicationController
 	def index
 		@metadata = []
-		5.times do
+		3.times do
 			@metadata << get_image_metadata_conf_pic
 		end
 
 		render 'index'
+	end
+
+	def serveSingleImage
+		metadata = get_image_metadata_conf_pic
+
+		render :json => {"blog_name": get_post_blog_name(metadata),
+				"post_url": get_post_url(metadata),
+				"image_url": get_post_image_url(metadata)}
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'cat_pages#index'
+
+  get '/cats/newimage', to: 'cat_pages#serveSingleImage'
 end
